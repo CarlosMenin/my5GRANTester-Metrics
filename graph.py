23dc6e26cd7f931bfce8bba25d29b901c16d9a70
CarlosMenin <92284071+CarlosMenin@users.noreply.github.com>
2023-07-01 12:25:56 -0300

    Add files via upload

--- graph.py
+++ graph.py
@@ -2,9 +2,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-base_filename = 'my5grantester_metrics_{}.csv'
+test = '10000'
+base_filename = 'my5grantester_metrics_{}.csv'.format(test)
 
-for delay_ms in range(100,501,100):
+colors = ['red', 'blue', 'green', 'orange', 'purple', 'yellow', 'cyan', 'magenta', 'gray', 'brown']
+
+for delay_ms in range(100, 101, 100):
     dados = pd.read_csv(base_filename.format(delay_ms))
     dados = dados.sort_values(by=dados.columns[0])
 
@@ -12,26 +15,38 @@ for delay_ms in range(100,501,100):
         first_line = True
         timestamp = np.array([])
         connections = np.array([])
-        grupo = dados.iloc[i:i+100].copy()
+        grupo = dados.iloc[i:i + 100].copy()
 
         for indice, linha in grupo.iterrows():
             if first_line:
                 timestamp_base = linha['timestamp']
-                first_line= False
-            
-            timestamp = np.append(timestamp,(linha['timestamp']-timestamp_base)/1000000000)
-            connections = np.append(connections,linha['SM5G_PDU_SESSION_ACTIVE_PENDING'])
-            
-        
-        cor = plt.scatter(timestamp,connections,label= "Tester {}".format(grupo.iloc[0,0])).get_facecolor()[0]
-        
-        # Traçar linha reta com valor médio
-        plt.axhline(y=np.mean(connections),color=cor)
-        plt.axhline(y=np.std(connections),color=cor,linestyle= '--')
+                first_line = False
 
-    plt.title('{}ms'.format(delay_ms))
-    plt.xlabel('Tempo do experimento (s)')
-    plt.ylabel('Tempo até conexão (ms)')
-    plt.legend()        
-    plt.savefig('graph_connections_{}ms.png'.format(delay_ms))
-    plt.clf()
+            timestamp = np.append(timestamp, (linha['timestamp'] - timestamp_base) / 1000000000)
+            connections = np.append(connections, linha['SM5G_PDU_SESSION_ACTIVE_PENDING'])
+
+        color_index = i // 100  # Índice da cor com base no grupo atual
+        cor = colors[color_index % len(colors)]
+
+        plt.scatter(timestamp, connections, label="Tester {}".format(color_index), color=cor)
+
+        # Calcular a média considerando NaN como zero
+        mean_value = np.nanmean(connections)
+        # Traçar linha reta com valor médio
+        plt.axhline(y=mean_value, color=cor, linestyle='-', label="Média")
+        plt.text(max(timestamp), mean_value, f'{mean_value:.2f}', ha='left', va='center')
+
+        # Calcular o desvio padrão
+        std_value = np.nanstd(connections)
+        # Traçar linha reta com desvio padrão
+        plt.axhline(y=mean_value + std_value, color=cor, linestyle='--', label="Desvio Padrão")
+        plt.axhline(y=mean_value - std_value, color=cor, linestyle='--')
+        plt.text(max(timestamp), mean_value + std_value, f'{mean_value+std_value:.2f}', ha='left', va='center')
+        plt.text(max(timestamp), mean_value - std_value, f'{mean_value-std_value:.2f}', ha='left', va='center')
+
+        plt.title('{}-{}'.format(test, color_index))
+        plt.xlabel('Tempo do experimento (s)')
+        plt.ylabel('Tempo até conexão (ms)')
+        plt.legend()
+        plt.savefig('graph_connections_{}-{}.png'.format(test, color_index))
+        plt.clf()
--- graph2.py
+++ graph2.py
@@ -2,44 +2,46 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-base_filename = 'my5grantester_metrics_{}.csv'
-colors = ['red','blue','green','orange','purple','gray']
-column_names = ['SM5G_PDU_SESSION_INACTIVE','MM5G_NULL','MM5G_DEREGISTERED','MM5G_REGISTERED_INITIATED','MM5G_REGISTERED','SM5G_PDU_SESSION_ACTIVE_PENDING']
+test = '10000'
+base_filename = 'my5grantester_metrics_{}.csv'.format(test)
+colors = ['red', 'blue', 'green', 'orange', 'purple', 'gray']
+column_names = ['SM5G_PDU_SESSION_INACTIVE', 'MM5G_NULL', 'MM5G_DEREGISTERED', 'MM5G_REGISTERED_INITIATED', 'MM5G_REGISTERED', 'SM5G_PDU_SESSION_ACTIVE_PENDING']
 
-for delay_ms in range(100,501,100):
+for delay_ms in range(100, 101, 100):
     dados = pd.read_csv(base_filename.format(delay_ms))
     dados = dados.sort_values(by=dados.columns[0])
 
-    for i in range(0, len(dados), 100):
-        first_line = True
+    for column_index, column_name in enumerate(column_names):
         ue_number = np.array([])
-        arrays = [[] for _ in range(len(column_names))]
+        column_data = np.array([])
 
         for indice, linha in dados.iterrows():
-            ue_number = np.append(ue_number,linha['gnbid'])
-            for j in range(len(column_names)):
-                arrays[j].append(linha[column_names[j]])
-        arrays = [np.array(arr) for arr in arrays]
-            
-    # Plotar as linhas
-    for j in range(0,len(column_names)):
-        plt.plot(ue_number,arrays[j],color= colors[j],label= column_names[j])
-        #valor_medio = np.mean(connections)
-        
-    # Traçar linha reta com valor médio
-    for j in range(0,len(column_names)):
-        mean = np.mean(arrays[j])
-        plt.axhline(y=mean,color= colors[j])
-    
-    # Plot da linha pontilhada com o desvio padrão
-    for j in range(0,len(column_names)):
-        std = np.std(arrays[j])
-        plt.axhline(y=std,color= colors[j],linestyle='--')
+            ue_number = np.append(ue_number, linha['gnbid'])
+            column_data = np.append(column_data, linha[column_name])
 
-    # Configurar título e legendas
-    plt.title('{}ms'.format(delay_ms))
-    plt.xlabel('UE Number')
-    plt.ylabel('Tempo de experimento (ms)')
-    plt.legend()     
-    plt.savefig('graph_times_{}ms.png'.format(delay_ms))
-    plt.clf()
+        # Plotar o gráfico da coluna atual
+        plt.plot(ue_number, column_data, color=colors[column_index], label=column_name)
+
+        # Calcular a média da coluna atual
+        mean_value = np.nanmean(column_data)
+        # Traçar linha reta com valor médio
+        plt.axhline(y=mean_value, color=colors[column_index], linestyle='-', label="Média")
+        plt.text(max(ue_number) + 55, mean_value, f'{mean_value:.2f}', ha='left', va='center')
+
+        # Calcular o desvio padrão da coluna atual
+        std_value = np.nanstd(column_data)
+        # Traçar linha pontilhada com desvio padrão
+        plt.axhline(y=mean_value + std_value, color=colors[column_index], linestyle='--', label="Desvio Padrão")
+        plt.axhline(y=mean_value - std_value, color=colors[column_index], linestyle='--')
+        plt.text(max(ue_number) + 55, mean_value + std_value, f'{mean_value+std_value:.2f}', ha='left', va='center')
+        plt.text(max(ue_number) + 55, mean_value - std_value, f'{mean_value-std_value:.2f}', ha='left', va='center')
+
+        # Configurar título e legendas do gráfico atual
+        plt.title('{} - {}'.format(test, column_name))
+        plt.xlabel('UE Number')
+        plt.ylabel('Tempo de experimento (ms)')
+        plt.legend()
+        
+        # Salvar o gráfico atual
+        plt.savefig('graph_times_{}_{}.png'.format(test, column_name))
+        plt.clf()
